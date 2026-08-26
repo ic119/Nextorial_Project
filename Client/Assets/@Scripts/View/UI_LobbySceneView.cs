67df7a312771891cfb89278a148e6be10fd67a1e
ickim <ickim@deltax.ai>
2026-08-26 15:45:44 +0900

    Update) 저장데이터 여부에 따라 이어하기 버튼 활성 & 비활성 로직 추가

--- Client/Assets/@Scripts/View/UI_LobbySceneView.cs
+++ Client/Assets/@Scripts/View/UI_LobbySceneView.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_LobbySceneView : MonoBehaviour
 {
     #region Variable
     [SerializeField] private GameObject maskImage;
+
+    [Header("Lobby Scene Buttons")]
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button continueButton;
+    [SerializeField] private Button exitButton;
     #endregion
 
     #region LifeCycle
-    private void Awake()
+private void Awake()
     {
         if (maskImage != null)
         {
             maskImage.SetActive(false);
+        }
+
+        if (continueButton != null)
+        {
+            bool hasSaveData = SaveDataController.Instance != null && SaveDataController.Instance.HasSaveData();
+            continueButton.gameObject.SetActive(hasSaveData);
         }
     }
     #endregion
